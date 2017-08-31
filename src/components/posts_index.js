16aa12425cda15438267516830d5398108cfa814
Vincent Tang <vincet3@uci.edu>
2017-08-30 18:49:59 -0700

    added action, route and reducer

---
+++ src/components/posts_index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions';
+
+class PostsIndex extends Component{
+    render(){
+        return (
+            <div>
+                Posts Index
+            </div>
+        );
+    }
+}
+
+export default connect(null,{fetchPosts})(PostsIndex);
