3f43ce8d68876f7be98a9df7a04ccd3521c75710
Vincent Tang <vincet3@uci.edu>
2017-08-31 19:52:25 -0700

    remove extra comments

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -6,10 +6,6 @@ export default function(state={},action){
         case DELETE_POST:
             return _.omit(state, action.payload);
         case FETCH_POST:
-            // const post = action.payload.data;
-            // const newState = {...state};
-            // newState[post.id] = post;
-            // return newState;
             const {data} = action.payload;
             return {...state,[data.id]:data}
         case FETCH_POSTS:
