6a62907d43b132456385bd59a47fe429e61cf762
Vincent Tang <vincet3@uci.edu>
2017-08-31 19:50:00 -0700

    added show page for each entry

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,17 @@
 import _ from 'lodash';
-import {FETCH_POSTS} from '../actions';
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from '../actions';
 
 export default function(state={},action){
     switch (action.type){
+        case DELETE_POST:
+            return _.omit(state, action.payload);
+        case FETCH_POST:
+            // const post = action.payload.data;
+            // const newState = {...state};
+            // newState[post.id] = post;
+            // return newState;
+            const {data} = action.payload;
+            return {...state,[data.id]:data}
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data,'id');
         default:
